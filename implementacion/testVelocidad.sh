4a464742ec8f6a701305508b439073ffa38dadbd
Joel <joel.e.camera@gmail.com>
2017-05-24 10:44:28 -0300

    modificaciones del test de velocidad

--- implementacion/testVelocidad.sh
+++ implementacion/testVelocidad.sh
@@ -8,21 +8,21 @@ touch testVelocidadGrandeRes.csv
 # Genero los tests
 python generarTestsVelocidad.py
 
-echo "TEST DE IMAGENES CHICAS"
-# Corro los tests de imagenes chicas y los agrego a testVelocidadChicoRes.csv
-for i in {1..41};
-    do
-    echo "test de imagenes chicas numero $i";
-    ./testvelocidad 50 < tests/testVelocidadRed$i.in >> testVelocidadChicoRes.csv;
-    echo "test de imagenes chicas numero $i TERMINADO";
-done;
-echo "test de imagenes chicas terminado"
+# echo "TEST DE IMAGENES CHICAS"
+# # Corro los tests de imagenes chicas y los agrego a testVelocidadChicoRes.csv
+# for i in {1..41};
+#     do
+#     echo "test de imagenes chicas numero $i";
+#     ./testvelocidad 50 < tests/testVelocidadRed$i.in >> testVelocidadChicoRes.csv;
+#     echo "test de imagenes chicas numero $i TERMINADO";
+# done;
+# echo "test de imagenes chicas terminado"
 
 echo "TEST DE IMAGENES GRANDES"
 # Corro los tests de imagenes grandes y los agrego a testVelocidadChicoRes.csv
 for i in {1..41};
     do
     echo "test grande numero $i";
-    ./testvelocidad 25 < tests/testVelocidad$i.in >> testVelocidadGrandeRes.csv;
+    ./testvelocidad 5 < tests/testVelocidad$i.in >> testVelocidadGrandeRes.csv;
     echo "test grande numero $i TERMINADO";
 done;
